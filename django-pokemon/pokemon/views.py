48ff7ee18d3123bfe99f2b15159ed7603d88e4de
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2020-07-22 22:56:11 +0200

    init api urls, views, serializers

--- django-pokemon/pokemon/views.py
+++ django-pokemon/pokemon/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from . import models
+from . import serializers
 
 # Create your views here.
+class PokemonView(viewsets.ModelViewSet):
+    queryset = models.Pokemon.objects.all()
+    serializer_class = serializers.PokemonSerializer
