ad9b11b6f6aaf45a473cd5e93b3a854f2bfcb073
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2020-07-22 22:54:04 +0200

    create pokemon model and init db migration

--- django-pokemon/pokemon/models.py
+++ django-pokemon/pokemon/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    type_1 = models.CharField(max_length=100)
+    type_2 = models.CharField(max_length=100, blank=True)
+    total = models.IntegerField()
+    hp = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    sp_attack = models.IntegerField()
+    sp_def = models.IntegerField()
+    speed = models.IntegerField()
+    generation = models.IntegerField()
+    legendary = models.BooleanField()
+    
