837c9f06b0edf7e8d83ad451ab7fb7cbc4d7504e
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2020-07-23 05:23:32 +0200

    create handler to check conditions

---
+++ django-pokemon/pokemon/handlers.py
@@ -0,0 +1,27 @@
+from .models import Pokemon
+from decimal import Decimal
+
+def handle_legendary(pokemon: Pokemon):
+    if pokemon.legendary == "True":
+        raise ValueError("Invalid pokemon type")
+
+def handle_ghost(pokemon: Pokemon):
+    if pokemon.has_type("Ghost"):
+        raise ValueError("Invalid pokemon type")
+
+def handle_steel(pokemon: Pokemon):
+    if pokemon.has_type("Steel"):
+        pokemon.hp = Decimal(pokemon.hp) * Decimal(2)
+
+def handle_fire(pokemon: Pokemon):
+    if pokemon.has_type("Fire"):
+        pokemon.attack = Decimal(pokemon.attack) * Decimal(0.9)
+
+def handle_bugAndFlying(pokemon: Pokemon):
+    if pokemon.has_type("Bug") and pokemon.has_type("Flying"):
+        pokemon.speed = Decimal(pokemon.speed) * Decimal(1.1)
+
+def handle_nameStartWithG(pokemon: Pokemon):
+    if pokemon.name[0] == "G":
+        len_name = len(pokemon.name)-1
+        pokemon.defense = Decimal(pokemon.defense) + Decimal(len_name*5)
