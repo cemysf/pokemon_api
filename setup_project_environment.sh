05e8ccc92ce9bc844c1e642d3a770ae95969abc6
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2020-07-23 06:45:15 +0200

    init development helper scripts

---
+++ setup_project_environment.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+VENV_PATH='virtualenv'
+PROJ_PATH='dummyproject'
+
+source ./$VENV_PATH/bin/activate
+
+# install app package
+python -m pip install django-pokemon/dist/django-pokemon-0.1.tar.gz
+
+# make changes
+sed -i '/INSTALLED_APPS = \[/a "pokemon",\n"rest_framework",' $PROJ_PATH/$PROJ_PATH/settings.py
+sed -i '/urlpatterns = \[/a path("pokemon/", include("pokemon.urls")),' $PROJ_PATH/$PROJ_PATH/urls.py
+sed -i '/urlpatterns = \[/i from django.urls import include' $PROJ_PATH/$PROJ_PATH/urls.py
+
+# init models
+python $PROJ_PATH/manage.py migrate
