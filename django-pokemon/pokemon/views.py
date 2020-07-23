7b277f804a6bcd2271d9b2d30aaaa7ba46b07c15
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2020-07-23 06:44:03 +0200

    implemented basic search

--- django-pokemon/pokemon/views.py
+++ django-pokemon/pokemon/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from . import models
 from . import serializers
 
 # Create your views here.
-class PokemonView(viewsets.ModelViewSet):
+class PokemonView(viewsets.ReadOnlyModelViewSet):
+    search_fields = ['name']
+    filter_backends = (filters.SearchFilter,)
     queryset = models.Pokemon.objects.all()
     serializer_class = serializers.PokemonSerializer
