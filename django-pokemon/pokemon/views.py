4faf0c85419fc960f08c73222fcc92315b67dc86
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2020-07-23 08:47:45 +0200

    filtering option for hp, attack, defense

--- django-pokemon/pokemon/views.py
+++ django-pokemon/pokemon/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters, permissions
+from rest_framework.filters import SearchFilter
 from . import models
 from . import serializers
-#from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
-# Create your views here.
+
 class PokemonView(viewsets.ReadOnlyModelViewSet):
     search_fields = ['name']
-    filter_backends = (filters.SearchFilter,)
+    filter_backends = (SearchFilter, DjangoFilterBackend)
     queryset = models.Pokemon.objects.all()
 
     serializer_class = serializers.PokemonSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    filter_fields = {
+        'hp': ['gte', 'lte', 'exact'],
+        'attack': ['gte', 'lte', 'exact'],
+        'defense': ['gte', 'lte', 'exact'],
+    }
