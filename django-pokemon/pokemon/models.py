110221271dd8b793118b2d55663131d18b8b575f
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2020-07-23 05:21:36 +0200

    change IntegerField to DecimalField in db model

--- django-pokemon/pokemon/models.py
+++ django-pokemon/pokemon/models.py
@@ -5,12 +5,27 @@ class Pokemon(models.Model):
     name = models.CharField(max_length=100)
     type_1 = models.CharField(max_length=100)
     type_2 = models.CharField(max_length=100, blank=True)
-    total = models.IntegerField()
-    hp = models.IntegerField()
-    attack = models.IntegerField()
-    defense = models.IntegerField()
-    sp_atk = models.IntegerField()
-    sp_def = models.IntegerField()
-    speed = models.IntegerField()
-    generation = models.IntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=3)
+    hp = models.DecimalField(max_digits=10, decimal_places=3)
+    attack = models.DecimalField(max_digits=10, decimal_places=3)
+    defense = models.DecimalField(max_digits=10, decimal_places=3)
+    sp_atk = models.DecimalField(max_digits=10, decimal_places=3)
+    sp_def = models.DecimalField(max_digits=10, decimal_places=3)
+    speed = models.DecimalField(max_digits=10, decimal_places=3)
+    generation = models.DecimalField(max_digits=10, decimal_places=3)
     legendary = models.BooleanField()
+
+    def has_type(self, type):
+        """Check both types of model
+
+        Args:
+            type (str): Pokemon type
+
+        Returns:
+            bool: True if pokemon has the given type indicator
+        """
+        if self.type_1 == type or self.type_2 == type:
+            return True
+        else:
+            return False
+    
