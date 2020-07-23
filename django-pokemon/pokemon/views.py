67c24a7455ccfd3abeb0c694246fd4a4aac8ffea
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2020-07-23 07:21:43 +0200

    add permission class to view

--- django-pokemon/pokemon/views.py
+++ django-pokemon/pokemon/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions
 from . import models
 from . import serializers
+#from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 class PokemonView(viewsets.ReadOnlyModelViewSet):
     search_fields = ['name']
     filter_backends = (filters.SearchFilter,)
     queryset = models.Pokemon.objects.all()
+
     serializer_class = serializers.PokemonSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
