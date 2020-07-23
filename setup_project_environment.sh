f9f2437aecbce58f899ac2c63f1c7e16482725cc
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2020-07-23 06:45:50 +0200

    add pagination option

--- setup_project_environment.sh
+++ setup_project_environment.sh
@@ -12,5 +12,11 @@ sed -i '/INSTALLED_APPS = \[/a "pokemon",\n"rest_framework",' $PROJ_PATH/$PROJ_P
 sed -i '/urlpatterns = \[/a path("pokemon/", include("pokemon.urls")),' $PROJ_PATH/$PROJ_PATH/urls.py
 sed -i '/urlpatterns = \[/i from django.urls import include' $PROJ_PATH/$PROJ_PATH/urls.py
 
+# pagination
+echo "REST_FRAMEWORK = {" >> $PROJ_PATH/$PROJ_PATH/settings.py
+echo "    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination'," >> $PROJ_PATH/$PROJ_PATH/settings.py
+echo "    'PAGE_SIZE': 10" >> $PROJ_PATH/$PROJ_PATH/settings.py
+echo "}" >> $PROJ_PATH/$PROJ_PATH/settings.py
+
 # init models
 python $PROJ_PATH/manage.py migrate
